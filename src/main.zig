f1304c180d18103215d3e20b9ef3dd999378c69b
sea gin <cijin@seagin.me>
2025-02-03 08:01:04 +0700

    add list of things to do

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
 pub fn main() !void {
+    // Todo:
+    // 1. Create "Server" -> recieves udp packets from "Client"
+    // 2. Write test to simulate client and server
+    // 3. Create udp socket and send "hello udp" from client to server
     std.debug.print("Welcome to kilogram protocol\n", .{});
 }
