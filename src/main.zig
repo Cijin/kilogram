a5c919240055a5a5b1b87b683e8134b6e9eeab03
sea gin <cijin@seagin.me>
2025-02-03 08:01:39 +0700

    just a line of comment

--- src/main.zig
+++ src/main.zig
@@ -5,5 +5,6 @@ pub fn main() !void {
     // 1. Create "Server" -> recieves udp packets from "Client"
     // 2. Write test to simulate client and server
     // 3. Create udp socket and send "hello udp" from client to server
+    // 4. I want to be able to visualize the whole thing? How?
     std.debug.print("Welcome to kilogram protocol\n", .{});
 }
