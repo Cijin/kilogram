1b3ff817fabec2797c10bb29aad27718b7a5c709
sea gin <cijin@seagin.me>
2025-02-04 19:28:27 +0700

    setup basic packet encoding

--- src/kilogram.zig
+++ src/kilogram.zig
@@ -1,20 +1,34 @@
 const std = @import("std");
+const mem = std.mem;
 
+const version: u8 = 1;
 const udp_mtu: u16 = 1460;
 const variance: u16 = 8;
 const header_size: u16 = 8 + 8 + 16 + variance;
 const data_buf_size = udp_mtu - header_size;
 
+const PacketSizeError = error{
+    TooBig,
+};
+
 const kilogram = struct {
     version: u8,
     encoding_type: u8,
-    len: u16,
+    data_length: u16,
     data: [data_buf_size]u8,
-
-    // encode
-    pub fn encode(d: []u8) void {
-        std.debug.print("{s}\n", .{d});
-    }
-
-    // frame
 };
+
+pub fn encode(d: []const u8) PacketSizeError!kilogram {
+    var data: [data_buf_size]u8 = undefined;
+    if (d.len > data_buf_size) {
+        return PacketSizeError.TooBig;
+    }
+    @memcpy(data[0..d.len], d);
+
+    return kilogram{
+        .version = version,
+        .encoding_type = 0,
+        .data_length = @intCast(d.len),
+        .data = data,
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
 
+const kg = @import("kilogram.zig");
+
 pub fn main() !void {
     // Todo:
     // 1. Create "Server" -> recieves udp packets from "Client"
     // 2. Write test to simulate client and server
     // 3. Create udp socket and send "hello udp" from client to server
     // 4. I want to be able to visualize the whole thing? How?
-    std.debug.print("Welcome to kilogram protocol\n", .{});
+    _ = try kg.encode("yellow");
 }
