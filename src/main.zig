22ad132d7e9f3571372fcd1af2308a130645e289
sea gin <cijin@seagin.me>
2025-02-06 09:11:07 +0700

    setup udp socket to recieve data

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,33 @@
 const std = @import("std");
+const net = std.net;
+const posix = std.posix;
 
 const kg = @import("kilogram.zig");
 
 pub fn main() !void {
-    // Todo:
-    // 1. Create "Server" -> recieves udp packets from "Client"
-    // 2. Write test to simulate client and server
-    // 3. Create udp socket and send "hello udp" from client to server
-    // 4. I want to be able to visualize the whole thing? How?
     _ = try kg.encode("yellow");
+
+    // Todo: create server code
+    // Client code switch from rev to write
+    const sockfd = posix.socket(posix.AF.INET, posix.SOCK.DGRAM, posix.IPPROTO.UDP) catch |err| {
+        std.debug.print("unable to start socket:{any}\n", .{err});
+        return;
+    };
+    defer posix.close(sockfd);
+
+    const addr = net.Address.parseIp("127.0.0.1", 42069) catch |err| {
+        std.debug.print("unable to parse ip:{any}\n", .{err});
+        return;
+    };
+    posix.bind(sockfd, &addr.any, addr.getOsSockLen()) catch |err| {
+        std.debug.print("unable to bind addr to socket:{any}\n", .{err});
+        return;
+    };
+
+    var buf: [1024]u8 = undefined;
+    const len = std.posix.recvfrom(sockfd, &buf, 0, null, null) catch |err| {
+        std.debug.print("error while recieving:{any}\n", .{err});
+        return;
+    };
+    std.debug.print("recv: {s}\n", .{buf[0..len]});
 }
